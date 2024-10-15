dcf368191d4783b20e40ca583bb02cdf345ac8d0
krishnakumar-M04 <krishnakumarm432@gmail.com>
2024-10-15 10:53:38 +0530

    redirect added

--- ActiveWeb/src/components/Testimonials.jsx
+++ ActiveWeb/src/components/Testimonials.jsx
@@ -26,22 +26,22 @@ const Testimonials = () => {
         className="mySwiper text-center mt-20"
           >
           
-              <SwiperSlide ><img src={john} alt="" className='w-[50px] h-[50px] ml-[735px] rounded-full ' />
+              <SwiperSlide ><img src={john} alt="" className='w-[50px] h-[50px] ml-[820px] rounded-full ' />
                   <h1 className='pt-4 text-xl font-semibold'>John</h1>
-              <p className='w-[600px] ml-[450px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
+              <p className='w-[600px] ml-[520px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
                   </p>
               </SwiperSlide>
-              <SwiperSlide><img src={winston} alt="" className='w-[50px] h-[50px] ml-[735px] rounded-full ' />
+              <SwiperSlide><img src={winston} alt="" className='w-[50px] h-[50px] ml-[810px] rounded-full ' />
               <h1 className='pt-4 text-xl font-semibold'>Winston</h1>
-              <p className='w-[600px] ml-[450px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
+              <p className='w-[600px] ml-[520px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
                   </p></SwiperSlide>
-              <SwiperSlide><img src={clare} alt="" className='w-[50px] h-[50px] ml-[735px] rounded-full ' />
+              <SwiperSlide><img src={clare} alt="" className='w-[50px] h-[50px] ml-[810px] rounded-full ' />
               <h1 className='pt-4 text-xl font-semibold'>Clare</h1>
-              <p className='w-[600px] ml-[450px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
+              <p className='w-[600px] ml-[520px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
                   </p></SwiperSlide>
-              <SwiperSlide className='mb-10'><img src={steven} alt="" className='w-[50px] h-[50px] ml-[735px] rounded-full ' />
+              <SwiperSlide className='mb-10'><img src={steven} alt="" className='w-[50px] h-[50px] ml-[810px] rounded-full ' />
               <h1 className='pt-4 text-xl font-semibold'>Steven</h1>
-              <p className='w-[600px] ml-[450px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
+              <p className='w-[600px] ml-[520px] mt-2'>“There live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”
                   </p></SwiperSlide>
                
       </Swiper>
