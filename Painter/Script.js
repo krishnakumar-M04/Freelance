c824e28f27bb0069007c0e3011b4a5da05981c62
krishnakumar-M04 <krishnakumarm432@gmail.com>
2024-10-18 12:59:57 +0530

    new project painter is addaed

---
+++ Painter/Script.js
@@ -0,0 +1,12 @@
+const makeAppoint = document.querySelector(".makeappoint-overlay");
+const popup = document.querySelector(".popup");
+
+function Show() {
+    makeAppoint.style.display = "block";
+    popup.style.display = "block";
+
+}
+function Close() {
+    makeAppoint.style.display = "none";
+    popup.style.display = "none";
+}
